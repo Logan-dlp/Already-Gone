ff8e98316e0b98829c831a6e9d08e7ca2f4fc514
Logan <pix.59700@gmail.com>
2025-06-03 12:50:46 +0200

    Final player Movement

--- Assets/_Core/Scripts/Players/Movements/PlayerMovementHandler.cs
+++ Assets/_Core/Scripts/Players/Movements/PlayerMovementHandler.cs
@@ -22,31 +22,17 @@ namespace AlreadyGone.Players.Movements
 
         private void FixedUpdate()
         {
-            if (Physics.Raycast(gameObject.transform.position, -gameObject.transform.up, out RaycastHit hitInfo, _gravityDistanceDetection) && hitInfo.transform != gameObject.transform)
-            {
-                _direction.y = 0;
-            } 
-            else {
-                _direction.y += Physics.gravity.y * Time.fixedDeltaTime;
-            }
-            
             ExecuteMovement();
         }
 
-        public void SetDirection(InputAction.CallbackContext ctx)
-        {
-            if (ctx.performed)
-            {
-                _direction = new Vector3(ctx.ReadValue<Vector2>().x, _direction.y, ctx.ReadValue<Vector2>().y);
-            }
-            else
-            {
-                _direction = new Vector3(0, _direction.y, 0);
-            }
-        }
+        public void SetDirection(InputAction.CallbackContext ctx) => _direction = new Vector3(ctx.ReadValue<Vector2>().x, 0, ctx.ReadValue<Vector2>().y);
 
         private void ExecuteMovement()
         {
+            _direction.y = Physics.Raycast(gameObject.transform.position, -gameObject.transform.up, out RaycastHit hitInfo, _gravityDistanceDetection)
+                                        && hitInfo.transform != gameObject.transform 
+                                ? 0 : _direction.y + Physics.gravity.y * Time.fixedDeltaTime;
+            
             if (_direction == Vector3.zero)
                 return;
             
